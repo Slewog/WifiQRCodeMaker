f5c281e142d417401833eca6daf23290ca13f0e9
Slewog <31140215+Slewog@users.noreply.github.com>
2025-11-27 19:34:30 +0100

    Win creation, load font
    - Get dir path to load assets.
    - Set appearance and theme.
    - Create basic window.
    - Load the custom font

--- main.py
+++ main.py
@@ -2,7 +2,38 @@ import sys
 import customtkinter as ctk
 from PIL import Image, ImageFont, ImageDraw
 
+from os.path import (
+    dirname    as os_path_dirname,
+    abspath    as os_path_abspath,
+    join       as os_path_join
+)
+
+DIR_PATH = getattr(sys, '_MEIPASS', os_path_dirname(os_path_abspath(__file__)))
+APP_TITLE = 'Wifi QR Code Maker'
+
+
+def get_screen_work_area():
+    pass
+
+
+class Gui:
+    def __init__(self, win: ctk.CTk) -> None:
+        self.win = win
+
+        self.win.title(APP_TITLE)
+        self.win.geometry('540x600')
+
 
 if __name__ == '__main__':
-    print('Hello World')
+    window = ctk.CTk()
+
+    ctk.set_default_color_theme("dark-blue")
+    ctk.set_appearance_mode('dark')
+    ctk.FontManager.load_font(os_path_join(DIR_PATH, 'Roboto.ttf'))
+
+    font12 = ctk.CTkFont(family='Roboto', size=12, weight='normal', slant='roman')
+    fontbold = ctk.CTkFont(family='Roboto', size=12, weight='bold', slant='roman')
+
+    app = Gui(win=window)
+    window.mainloop()
     sys.exit()
