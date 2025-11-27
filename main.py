443f4881079e3dbf83f85573a09a3b1d1f2dac83
Slewog <31140215+Slewog@users.noreply.github.com>
2025-11-27 21:36:51 +0100

    Update main.py
    - Check file exists before loading.
    - Improve get_display_work_area for windows (need make test about linux/mac version)

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from PIL import Image, ImageFont, ImageDraw
 from os.path import (
     dirname    as os_path_dirname,
     abspath    as os_path_abspath,
-    join       as os_path_join
+    join       as os_path_join,
+    exists     as os_path_exists
 )
 
 DIR_PATH = getattr(sys, '_MEIPASS', os_path_dirname(os_path_abspath(__file__)))
@@ -13,20 +14,21 @@ ASSETS_PATH = os_path_join(DIR_PATH, 'assets')
 APP_TITLE = 'Wifi QR Code Maker'
 
 
-def get_display_work_area(is_win32:bool) -> dict[str, int]:
+def get_display_work_area(is_win32: bool) -> dict[str, int]:
     if is_win32:
         from ctypes import windll, byref
         from ctypes.wintypes import RECT
+        SPI_GETWORKAREA = 0x0030
         work_area = RECT()
-        _ = windll.user32.SystemParametersInfoW(0x0030, 0, byref(work_area), 0)
+        _ = windll.user32.SystemParametersInfoW(SPI_GETWORKAREA, 0, byref(work_area), 0)
 
-        return {'width': work_area.right, 'height': work_area.bottom}
+        return {'width': work_area.right - work_area.left, 'height': work_area.bottom - work_area.top}
 
     from re import split
     from subprocess import check_output
     work_area = split(
         '=|,',
-        check_output(['xprop', '-root', '_NET_WORKAREA']).decode()
+        check_output(['xprop', '-root', '_NET_WORKAREA']).decode(errors='ignore')
     )
 
     return {'width': int(work_area[3]), 'height': int(work_area[4])}
@@ -42,7 +44,9 @@ class Gui:
     def __init__(self, win: ctk.CTk, is_win32:bool, work_area:dict[str, int]) -> None:
         win.title(APP_TITLE)
         if is_win32:
-            win.iconbitmap(os_path_join(ASSETS_PATH, 'images', 'logo.ico'))
+            icon_path = os_path_join(ASSETS_PATH, 'images', 'logo.ico')
+            if os_path_exists(icon_path):
+                win.iconbitmap(icon_path)
         win.geometry(make_geometry(work_area, 540, 600))
         win.resizable(width=False, height=False)
 
@@ -53,10 +57,15 @@ if __name__ == '__main__':
 
     ctk.set_default_color_theme("dark-blue")
     ctk.set_appearance_mode('dark')
-    ctk.FontManager.load_font(os_path_join(DIR_PATH, 'Roboto.ttf'))
 
-    font12 = ctk.CTkFont(family='Roboto', size=12, weight='normal', slant='roman')
-    fontbold = ctk.CTkFont(family='Roboto', size=12, weight='bold', slant='roman')
+    font_path = os_path_join(ASSETS_PATH, 'fonts','Roboto.ttf')
+    font_loaded = False
+    if os_path_exists(font_path):
+        ctk.FontManager.load_font(os_path_join(ASSETS_PATH, 'fonts','Roboto.ttf'))
+        font_loaded = True
+    font_family = 'Roboto' if font_loaded else 'Segoe UI'
+    font12 = ctk.CTkFont(family=font_family, size=12, weight='normal', slant='roman')
+    fontbold = ctk.CTkFont(family=font_family, size=12, weight='bold', slant='roman')
 
     is_win32 = sys.platform.startswith('win')
     app = Gui(window, is_win32, get_display_work_area(is_win32))
